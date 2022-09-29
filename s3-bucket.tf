48a48fb906cf77a81645d48f9812aeb4e689d952
aayush <50005501@stu.upes.ac.inm>
2022-09-29 12:09:49 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,6 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/testaayush/s3-bucket/aws"
   version = "2.8.0"
   bucket = "my-s3-bucket"
+  bucket_prefix = "aayush"
   acl    = "private"
 
   versioning = {
